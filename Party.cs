297497aa1a50bd244a98866f7be22bd720b8952c
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-30 14:33:44 -0500

    Fix syntax issues in Party class
    Refactor Party class to correct syntax errors.

--- Party.cs
+++ Party.cs
@@ -3,6 +3,6 @@ public class Party
 {
 public Person(string firstName, string lastName){}
 public string[] Guests(){
-return new string[Person(sdliubg, sogb), Person(sdliubg, sogb), Person(sdliubg, sogb)];
+return new string[Person(sdliubg sogb), Person(sdliubg sogb), Person(sdliubg sogb)];
 }
 }
