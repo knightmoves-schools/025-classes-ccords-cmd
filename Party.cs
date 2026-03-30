d2b97b5c0bcf6d67677316afd9a072c8be3510e4
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-30 15:07:41 -0500

    Update Party.cs

--- Party.cs
+++ Party.cs
@@ -5,11 +5,10 @@ public Person[] Guests(){
  new Person(sdliubg, sogb);
  new Person(sdliubg, sogb);
  new Person(sdliubg, sogb);
-}
-}
+}}
 public class Person{
  public string Firstname = {get;}
- public string Lastname = {get}
+ public string Lastname = {get;}
  public Person(string firstName, string lastName){
  Firstname = firstName;
  Lastname = lastName;
