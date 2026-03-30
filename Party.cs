cf7edc5361385d5a26aa0ac86180b1283f173af9
ccords-cmd <ccords@anamosa.k12.ia.us>
2026-03-30 15:15:00 -0500

    Update Party.cs

--- Party.cs
+++ Party.cs
@@ -1,21 +1,22 @@
 ﻿namespace knightmoves;
 public class Party
 {
-public Person[] Guests = new Person[]{
- new Person("sdliubg", "sogb"),
- new Person("sdliubg", "sogb"),
- new Person("sdliubg", "sogb")
-};
+    public Person[] Guests = new Person[]
+    {
+        new Person("John", "Doe"),
+        new Person("Jane", "Smith"),
+        new Person("Bob", "Johnson")
+    };
 }
 
 public class Person
 {
- public string FirstName = { get; }
- public string LastName = { get; }
- 
- public Person(string firstName, string lastName)
- {
- FirstName = firstName;
- LastName = lastName;
- }
+    public string FirstName { get; }
+    public string LastName { get; }
+
+    public Person(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
 }
